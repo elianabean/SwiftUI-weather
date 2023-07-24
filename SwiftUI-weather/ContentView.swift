b80c034de59164b562270c72d7239576899f9b5b
Eliana <ewanginspiration@gmail.com>
2023-07-24 11:52:53 -0400

    Initial Commit

---
+++ SwiftUI-weather/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI-weather
+//
+//  Created by Eliana Wang on 7/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-weather/SwiftUI_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_weatherApp.swift
+//  SwiftUI-weather
+//
+//  Created by Eliana Wang on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
