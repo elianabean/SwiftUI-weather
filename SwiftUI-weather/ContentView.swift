135680dd51fadd845ec45b4944bf5064866d1a30
Eliana <ewanginspiration@gmail.com>
2023-07-26 15:14:48 -0400

    created background gradient and added city name

--- SwiftUI-weather/ContentView.swift
+++ SwiftUI-weather/ContentView.swift
@@ -9,14 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            // colors: [array of colors]
+            //edgesIgnoringSafeArea(.all) will cover the ENTIRE screen
+            
+            VStack {
+                Text("Cupertino, CA")
+                    //the order of modifiers matters
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white) //text color
+                    .padding()
+                Spacer()
+                    
+            }
+            
+            
+            }
         }
-        .padding()
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
