f523378144759f4b156d7d79da98cdc9d8898396
Eliana <ewanginspiration@gmail.com>
2023-10-11 18:09:57 -0400

    added button and refactored

--- SwiftUI-weather/ContentView.swift
+++ SwiftUI-weather/ContentView.swift
@@ -4,36 +4,24 @@
 //
 //  Created by Eliana Wang on 7/24/23.
 //
+// Using Sean Allen tutorial: https://youtu.be/HXoVSbwWUIk?si=yvGv_ft5Ql_JDLKo 
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isNight = false
+    
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, Color("Light Blue")]),
-                           startPoint: .topLeading,
-                           endPoint: .bottomTrailing)
-                .edgesIgnoringSafeArea(.all)
+            BackgroundView(isNight: $isNight) //$ makes it binding. it passes in the isNight in ContentView as a parameter to isNight in BackgroundView
             // colors: [array of colors]
             //edgesIgnoringSafeArea(.all) will cover the ENTIRE screen
+            //ternary operators: (condition) ? (if true) : (if false)
             
             VStack {
-                Text("Cupertino, CA")
-                    //the order of modifiers matters
-                    .font(.system(size: 32, weight: .medium, design: .default))
-                    .foregroundColor(.white) //text color
-                    .padding()
-                VStack(spacing: 10) {
-                    Image(systemName: "cloud.sun.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode:.fit) //keeps the ratio of the image, but you can resize the frame
-                        .frame(width:180,height: 180)
-                    Text("76°")
-                        .font(.system(size:70, weight:.medium))
-                        .foregroundColor(.white)
-                }
-                .padding(.bottom, 40) //padding under the VStack
+                CityTextView(cityName: "Cuppertino, California")
+                
+                MainWeatherStatusView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill", temperature: 76)
                 
                 
                 HStack (spacing:20) {
@@ -58,6 +46,17 @@ struct ContentView: View {
                                    temperature: 25)
                 }
                 Spacer()
+                
+                Button {
+                    //action
+                    isNight.toggle()
+                    
+                } label: {
+                    //what the button looks like
+                    WeatherButton(title: "Change Day Time", textColor: .blue, backgroundColor: .white)
+                }
+                
+                Spacer() //spacer counts as a view. you can have 10 views in total in 1 page (nested views count as 1)
                     
             }
             
@@ -94,5 +93,65 @@ struct WeatherDayView: View {
                 .font(.system(size:28, weight:.medium))
                 .foregroundColor(.white)
         }
+    }
+}
+
+struct BackgroundView: View {
+    @Binding var isNight: Bool
+    
+    var body: some View {
+        
+        
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue, isNight ? .gray : Color("lightBlue")]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CityTextView: View {
+    var cityName: String
+    
+    var body: some View {
+        Text(cityName)
+            //the order of modifiers matters
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white) //text color
+            .padding()
+        
+    }
+}
+
+struct MainWeatherStatusView: View {
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode:.fit) //keeps the ratio of the image, but you can resize the frame
+                .frame(width:180,height: 180)
+            Text("\(temperature)°")
+                .font(.system(size:70, weight:.medium))
+                .foregroundColor(.white)
+        }
+        .padding(.bottom, 40) //padding under the VStack
+    }
+}
+
+struct WeatherButton: View {
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width:200, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size:20, weight:.bold, design:.default))
+            .cornerRadius(10.0)
     }
 }
